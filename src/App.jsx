e0966fcf2f7d4f10b375216c3862c794a132f99c
Dino Musić <dmusic996@gmail.com>
2023-09-20 19:05:24 +0200

    ispravak

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Component, useState } from 'react';
+import {UserClass, UserChildren, UserFunction} from './Users';
+import React from 'react';
 
-function App() {
-  const niz = ["jedan", "dva", "tri"]
-    return(
-      <>
-      {
-        niz.map(stringBroj => <li>{stringBroj}</li>)
-      }
-      </>
-  )
+export default function App() {
+  const initialUsers = [
+    { id:1, name: 'Dino', age: 27 },
+    { id:2, name: 'Matej', age: 34 },
+    { id:3, name: 'Mirta', age: 32 },
+  ];
+
+  const tekst = "Uvećaj godine";
+
+  const [users, setUsers] = useState(initialUsers);
+  const [newName, setNewName] = useState(""); 
+  const [selectedUserIndex, setSelectedUserIndex] = useState(0);
+
+  const uvecajGodine = () => {
+    setUsers(prevState => {
+      const newUsers = prevState.map (user => {
+        return {...user, age: user.age + 1 }
+    })
+    return newUsers
+    })
 }
 
-export default App
+const promijeniIme = () => {
+  setUsers((prevState) =>
+    prevState.map((user, index) => {
+      if (index === selectedUserIndex) {
+        return { ...user, name: newName };
+      }
+      return user;
+    })
+  );
+};
+
+const dodajNovogKorisnika = () => {
+  const noviKorisnik = {id: users.length + 1, name: newName, age: 0}
+  setUsers((prevState) => [...prevState, noviKorisnik]);
+};
+
+  return (
+    <>
+      <h1>State</h1>
+      <input 
+        type="text"
+        value={newName}
+        onChange={(Event) => setNewName(Event.target.value)}
+       />
+       <button onClick={promijeniIme}>Upiši novo ime</button>
+       <button onClick={dodajNovogKorisnika}>Dodaj novog korisnika</button>
+
+       {users.map((user, index) => (
+        <div key={user.id}>
+          <UserFunction name={user.name} age={user.age} />
+        </div>
+      ))}
+      <button onClick={uvecajGodine}>Godina više</button>
+    </>
+  ); 
+};
+
